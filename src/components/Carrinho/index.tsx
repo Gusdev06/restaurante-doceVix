cd1bc05c1019e3c9b1f92891abafbadb5b8fb9b2
Gustavo Gomes <123559372+Gusdev06@users.noreply.github.com>
2023-09-04 16:05:31 -0300

    style: estilizando modal carrinho

--- src/components/Carrinho/index.tsx
+++ src/components/Carrinho/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { CardCarrinho, ModalButton, ModalStyleCart, ModalTitle } from "./styles"
+import * as S from "./styles";
 import Modal from "react-modal";
 Modal.setAppElement("#root");
 
@@ -20,19 +20,95 @@ const Carrinho = () => {
     return (
         <>
 
-        <CardCarrinho onClick={AbrirModal}>
+        <S.CardCarrinho onClick={AbrirModal}>
         <span>Meu carrinho(4)</span>
         <span>R$79,00</span>
-        </CardCarrinho>
-        <ModalStyleCart isOpen={modalIsOpen} onRequestClose={FecharModal}>
-            <div>
-                <ModalTitle>Carrinho</ModalTitle>
-                <ModalButton>Retirada no balcão</ModalButton>
-                <ModalButton>Delivery</ModalButton>
-                
-            </div>
-        </ModalStyleCart>
+        </S.CardCarrinho>
+        <S.ModalStyleCart isOpen={modalIsOpen} onRequestClose={FecharModal}>
+            <div style={{ borderBottom: "1px solid #E1E1E1", marginBottom: "24px"}}>
+                <div style={{display: "flex", justifyContent: "space-between", borderBottom: "1px solid #E1E1E1", alignItems: "center", paddingBottom: "20px"}}>
+                    <S.BsCart4Style></S.BsCart4Style>
+                    <S.ModalTitle>Carrinho</S.ModalTitle>
+                    <S.FiXStyle onClick={FecharModal}></S.FiXStyle>
+    
+                </div>
+            </div >
+            <S.ModalItens>
+                <span>1x</span>
+                <span>Marmitex Médio Strogonoff de frango</span>
+                <b>R$21,00</b>
+                    <button type="submit">
+                    <S.BsTrash3Style />
+                    </button>
+             
+            </S.ModalItens>
 
+            <S.ModalItens>
+                <span>1x</span>
+                <span>Marmitex Médio Strogonoff de frango</span>
+                <b>R$21,00</b>
+                
+                <button type="submit">
+                    <S.BsTrash3Style />
+                    </button>
+            </S.ModalItens>
+
+            <S.ModalItens>
+                <span>1x</span>
+                <span>Marmitex Médio Strogonoff de frango</span>
+                <b>R$21,00</b>
+                
+                <button type="submit">
+                    <S.BsTrash3Style />
+                    </button>
+            </S.ModalItens>
+
+            <S.ModalPayment>
+                    <div style={{display: "flex", justifyContent: "space-between", borderBottom: "1px solid #E1E1E1", alignItems: "center", paddingBottom: "10px"}}>
+                        <S.BsWallet2Style></S.BsWallet2Style>
+                        <S.ModalTittlePay>Forma de pagamento</S.ModalTittlePay>
+                        <span></span>
+                    </div>  
+                <S.ModalForm>
+                    <S.ModalLabel>
+                        <input type="radio" name="payment" value="cartao" />
+                        Máquina de cartão
+                    </S.ModalLabel>    
+        
+                    <S.ModalLabel>
+                        <input type="radio" name="payment" value="dinheiro" />
+                        Dinheiro
+                    </S.ModalLabel>
+
+                    <S.ModalLabel>
+                        <input type="radio" name="payment" value="pix" />
+                        Pix
+                    </S.ModalLabel>
+                </S.ModalForm>
+
+                <div style={{display: "flex", justifyContent: "space-between", borderBottom: "1px solid #E1E1E1", alignItems: "center", paddingBottom: "10px", marginTop: "15px"}}>
+                        <S.BsWallet2Style></S.BsWallet2Style>
+                        <S.ModalTittlePay>Entrega</S.ModalTittlePay>
+                        <span></span>
+                    </div>  
+                <S.ModalForm>
+                    <S.ModalLabel>
+                        <input type="radio" name="entrega" value="retirada" />
+                        Retirada no balcão
+                    </S.ModalLabel>    
+        
+                    <S.ModalLabel>
+                        <input type="radio" name="entrega" value="delivey" />
+                        Delivey
+                    </S.ModalLabel>
+                </S.ModalForm>
+                <S.ModalPrice>
+                    <span>TOTAL</span>
+                    <span>R$64,00</span>
+                </S.ModalPrice>
+    
+            </S.ModalPayment>
+        </S.ModalStyleCart>
         </>
 
 
--- src/components/Carrinho/styles.ts
+++ src/components/Carrinho/styles.ts
@@ -1,5 +1,34 @@
 import styled from 'styled-components' 
 import {ModalStyle} from '../CardFood/styles'
+import { BsCart4, BsTrash3, BsWallet2 } from "react-icons/bs";
+import { FiX } from "react-icons/fi";
+
+
+export const BsTrash3Style = styled(BsTrash3)`
+color: #EB3223;
+font-size: 14px;
+`
+
+
+export const FiXStyle = styled(FiX)`
+  cursor: pointer;
+  transition: all 0.1s ease;
+  font-size: 25px;
+  &:hover {
+    color: #c3c3c3;
+  }
+`;
+
+
+export const BsCart4Style = styled(BsCart4)`
+color: #000;
+font-size: 30px;
+
+`
+
+export const BsWallet2Style = styled(BsWallet2)`
+color: #000;
+`
 
 
 export const CardCarrinho = styled.div`
@@ -20,19 +49,76 @@ border-radius: 20px 20px 0px 0px;
 export const ModalStyleCart = styled(ModalStyle)`
 padding: 40px;
 height: 700px;
-text-align: center;
+
 `
 
 export const ModalTitle = styled.h1 `
-border-bottom: 1px #E1E1E1 solid;
-padding-bottom: 28px;
+font-size: 25px;
+
+
+
 `
 
 export const ModalButton = styled.div`
 margin-top: 24px;
+margin-bottom: 24px;
 padding: 9px;
 text-align: center;
 border-radius: 10px;
 border: 1px solid #E1E1E1;
 `
+
+export const ModalItens = styled.div`
+display: flex;
+justify-content: space-between;
+padding: 5px;
+margin-bottom: 24px;
+border-bottom: 1px solid #E1E1E1;
+`
+
+export const ModalPayment = styled.div`
+padding-top: 44px;
+margin-bottom: 30px;
+`
+
+export const ModalTittlePay = styled.h3 `
+font-size: 16px;
+text-align: center;
+font-weight: bold;
+
+
+
+`
+export const ModalLabel= styled.label`
+display: flex;
+gap: 8px;
+padding-bottom: 10px;
+font-size: 16px;
+
+`
+
+export const ModalForm = styled.form`
+margin-top: 14px;
+
+`
+
+
+
+export const ModalPrice = styled.div `
+padding-top: 25px;
+padding-bottom: 10px;
+font-weight: bold;
+display: flex;
+justify-content: space-between;
+border-bottom: 1px solid #E1E1E1;
+`
+
+
+
+
+
+
+
+
+
 
